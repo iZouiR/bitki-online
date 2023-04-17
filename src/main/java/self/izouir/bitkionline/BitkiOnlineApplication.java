ef51e1eac8727fa6a002bd5cf0799dfe980eec4d
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-17 17:16:44 +0300

    init commit

---
+++ src/main/java/self/izouir/bitkionline/BitkiOnlineApplication.java
@@ -0,0 +1,13 @@
+package self.izouir.bitkionline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitkiOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitkiOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/self/izouir/bitkionline/BitkiOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package self.izouir.bitkionline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitkiOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
