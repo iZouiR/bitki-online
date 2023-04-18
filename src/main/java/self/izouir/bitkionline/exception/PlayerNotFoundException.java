b193541fda86c7da6a9ee9f8d7cea82824ad2782
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-19 01:12:51 +0300

    add exception package: add PlayerNotFoundException

---
+++ src/main/java/self/izouir/bitkionline/exception/PlayerNotFoundException.java
@@ -0,0 +1,7 @@
+package self.izouir.bitkionline.exception;
+
+public class PlayerNotFoundException extends RuntimeException {
+    public PlayerNotFoundException(String message) {
+        super(message);
+    }
+}
