0686e5ecdf9a53bd8adba9ea1382c7b1778c5aa7
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-19 02:26:02 +0300

    update PlayerBotService: add findByPlayerId, existsByPlayerId, save methods

--- src/main/java/self/izouir/bitkionline/service/player/PlayerBotService.java
+++ src/main/java/self/izouir/bitkionline/service/player/PlayerBotService.java
@@ -2,7 +2,11 @@ package self.izouir.bitkionline.service.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import self.izouir.bitkionline.entity.player.PlayerBot;
+import self.izouir.bitkionline.exception.PlayerBotNotFoundException;
 import self.izouir.bitkionline.repository.player.PlayerBotRepository;
+
+import java.util.Optional;
 
 @Service
 public class PlayerBotService {
@@ -11,5 +15,18 @@ public class PlayerBotService {
     @Autowired
     public PlayerBotService(PlayerBotRepository playerBotRepository) {
         this.playerBotRepository = playerBotRepository;
+    }
+
+    public PlayerBot findByPlayerId(Long playerId) {
+        Optional<PlayerBot> optional = playerBotRepository.findByPlayerId(playerId);
+        return optional.orElseThrow(() -> new PlayerBotNotFoundException("Player bot with playerId = " + playerId + " was not found"));
+    }
+
+    public boolean existsByPlayerId(Long playerId) {
+        return playerBotRepository.findByPlayerId(playerId).isPresent();
+    }
+
+    public void save(PlayerBot playerBot) {
+        playerBotRepository.save(playerBot);
     }
 }
