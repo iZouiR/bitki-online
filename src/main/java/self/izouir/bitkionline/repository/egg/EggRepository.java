3010c9591c52f8bcb850f2401eba710c2bd7120c
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-20 00:13:05 +0300

    update EggRepository: add deleteAllByOwner method

--- src/main/java/self/izouir/bitkionline/repository/egg/EggRepository.java
+++ src/main/java/self/izouir/bitkionline/repository/egg/EggRepository.java
@@ -3,7 +3,9 @@ package self.izouir.bitkionline.repository.egg;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import self.izouir.bitkionline.entity.egg.Egg;
+import self.izouir.bitkionline.entity.player.Player;
 
 @Repository
 public interface EggRepository extends JpaRepository<Egg, Long> {
+    void deleteAllByOwner(Player owner);
 }
