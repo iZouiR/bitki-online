d86c32209272cfa147dba742240f5d8d3d9865da
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-19 15:41:37 +0300

    add ImageNotFoundException

---
+++ src/main/java/self/izouir/bitkionline/exception/ImageNotFoundException.java
@@ -0,0 +1,7 @@
+package self.izouir.bitkionline.exception;
+
+public class ImageNotFoundException extends RuntimeException {
+    public ImageNotFoundException(String message) {
+        super(message);
+    }
+}
