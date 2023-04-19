5bb8d361000197ff069ad30f61fb831da9595a8e
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-20 00:25:51 +0300

    update ProfileCommander: add processing callback queries implementation, changeUsername + dropPlayer + getPlayerInfo + get-keyboard markup methods

--- src/main/java/self/izouir/bitkionline/commander/ProfileCommander.java
+++ src/main/java/self/izouir/bitkionline/commander/ProfileCommander.java
@@ -2,7 +2,9 @@ package self.izouir.bitkionline.commander;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import self.izouir.bitkionline.bot.DispatcherBot;
 import self.izouir.bitkionline.entity.player.BotState;
 import self.izouir.bitkionline.entity.player.Player;
@@ -12,7 +14,11 @@ import self.izouir.bitkionline.service.player.PlayerService;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
+import static self.izouir.bitkionline.commander.util.BotCommander.sendEditMessageText;
 import static self.izouir.bitkionline.commander.util.BotCommander.sendMessage;
 
 @Component
@@ -31,18 +37,82 @@ public class ProfileCommander {
         this.eggCommander = eggCommander;
     }
 
+    public void processCallbackQuery(DispatcherBot bot, Long chatId, Integer messageId, String callbackData) {
+        Player player = playerService.findByChatId(chatId);
+
+        switch (callbackData) {
+            case "CHANGE_USERNAME" -> {
+                EditMessageText message = EditMessageText.builder()
+                        .chatId(String.valueOf(chatId))
+                        .messageId(messageId)
+                        .text("Ok, please. Enter your new username")
+                        .build();
+
+                PlayerBot playerBot = playerBotService.findByPlayerId(player.getId());
+                playerBot.setLastBotState(BotState.CHANGE_USERNAME);
+                playerBotService.save(playerBot);
+
+                sendEditMessageText(bot, message);
+            }
+            case "REFRESH_EGGS" -> {
+                EditMessageText message = EditMessageText.builder()
+                        .chatId(String.valueOf(chatId))
+                        .messageId(messageId)
+                        .text("Are you sure you want to drop your eggs and pick new ones?")
+                        .build();
+                message.setReplyMarkup(getRefreshEggsInlineKeyboardMarkup());
+                sendEditMessageText(bot, message);
+            }
+            case "CONFIRM_REFRESH_EGGS" -> {
+                eggCommander.deleteAllPlayersEggs(player);
+                EditMessageText message = EditMessageText.builder()
+                        .chatId(String.valueOf(chatId))
+                        .messageId(messageId)
+                        .text("All your eggs were dropped and now will be refreshed")
+                        .build();
+                sendEditMessageText(bot, message);
+                eggCommander.generateStarterEggs(bot, chatId, player);
+            }
+            case "DROP_PROFILE" -> {
+                EditMessageText message = EditMessageText.builder()
+                        .chatId(String.valueOf(chatId))
+                        .messageId(messageId)
+                        .text("Are you sure you want to drop your whole profile?")
+                        .build();
+                message.setReplyMarkup(getDropProfileInlineKeyboardMarkup());
+                sendEditMessageText(bot, message);
+            }
+            case "CONFIRM_DROP_PROFILE" -> {
+                dropPlayer(player);
+                EditMessageText message = EditMessageText.builder()
+                        .chatId(String.valueOf(chatId))
+                        .messageId(messageId)
+                        .text("Your profile was deleted, now you can register another one - /start")
+                        .build();
+                sendEditMessageText(bot, message);
+            }
+            case "DENY_REFRESH_EGGS", "DENY_DROP_PROFILE" -> {
+                EditMessageText message = EditMessageText.builder()
+                        .chatId(String.valueOf(chatId))
+                        .messageId(messageId)
+                        .text(getPlayerInfo(player))
+                        .build();
+                message.setReplyMarkup(getProfileInlineKeyboardMarkup());
+                sendEditMessageText(bot, message);
+            }
+        }
+    }
+
     // register player when entering username
     // * true, if expected operation was registration
     // * false, if expected operation was not registration
-    public boolean register(DispatcherBot dispatcherBot, Update update, String username) {
-        Long chatId = update.getMessage().getChatId();
-
+    public boolean register(DispatcherBot bot, Long chatId, String username) {
         if (playerService.existsByChatId(chatId)) {
             Player player = playerService.findByChatId(chatId);
             if (playerBotService.existsByPlayerId(player.getId())) {
                 PlayerBot playerBot = playerBotService.findByPlayerId(player.getId());
                 if (playerBot.getLastBotState() == BotState.AWAIT_USERNAME) {
-                    if (!playerService.existsByUsernameIgnoreCase(username)) {
+                    if (playerService.notExistsByUsernameIgnoreCase(username)) {
                         player.setUsername(username);
                         player.setRank(0L);
                         player.setRegisteredAt(Timestamp.from(Instant.now()));
@@ -51,16 +121,119 @@ public class ProfileCommander {
                         playerBot.setLastBotState(null);
                         playerBotService.save(playerBot);
 
-                        sendMessage(dispatcherBot, chatId, "Congratulations, you are now registered with username " + username + "!");
+                        sendMessage(bot, chatId, "Congratulations, you are now registered with username " + username + "!");
 
-                        eggCommander.generateStarterEggs(dispatcherBot, update, player);
+                        eggCommander.generateStarterEggs(bot, chatId, player);
                     } else {
-                        sendMessage(dispatcherBot, chatId, "Player with username " + username + " already exists, try another variant");
+                        sendMessage(bot, chatId, "Player with username " + username + " already exists, try another variant");
                     }
                     return true;
                 }
             }
         }
         return false;
+    }
+
+    public boolean changeUsername(DispatcherBot bot, Long chatId, String username) {
+        if (playerService.existsByChatId(chatId)) {
+            Player player = playerService.findByChatId(chatId);
+            if (playerBotService.existsByPlayerId(player.getId())) {
+                PlayerBot playerBot = playerBotService.findByPlayerId(player.getId());
+                if (playerBot.getLastBotState() == BotState.CHANGE_USERNAME) {
+                    if (playerService.notExistsByUsernameIgnoreCase(username)) {
+                        player.setUsername(username);
+                        playerService.save(player);
+
+                        playerBot.setLastBotState(null);
+                        playerBotService.save(playerBot);
+
+                        sendMessage(bot, chatId, "Success, you are now named " + username);
+                    } else {
+                        sendMessage(bot, chatId, "Player with username " + username + " already exists, try another variant");
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void dropPlayer(Player player) {
+        eggCommander.deleteAllPlayersEggs(player);
+        playerBotService.deleteByPlayerId(player.getId());
+        playerService.delete(player);
+    }
+
+    public String getPlayerInfo(Player player) {
+        return "Username: " + player.getUsername() + "\n" +
+               "Rank: " + player.getRank() + " points\n" +
+               "Registered: " + player.getRegisteredAt().toLocalDateTime().format(DateTimeFormatter.ofPattern("dd.MM.yyyy [hh:mm]")) + "\n";
+    }
+
+    public InlineKeyboardMarkup getProfileInlineKeyboardMarkup() {
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+
+        List<InlineKeyboardButton> changeUsernameRow = new ArrayList<>();
+        InlineKeyboardButton changeUsernameButton = new InlineKeyboardButton();
+        changeUsernameButton.setText("Change username");
+        changeUsernameButton.setCallbackData("CHANGE_USERNAME");
+        changeUsernameRow.add(changeUsernameButton);
+
+        List<InlineKeyboardButton> refreshEggsRow = new ArrayList<>();
+        InlineKeyboardButton refreshEggsButton = new InlineKeyboardButton();
+        refreshEggsButton.setText("Refresh eggs");
+        refreshEggsButton.setCallbackData("REFRESH_EGGS");
+        refreshEggsRow.add(refreshEggsButton);
+
+        List<InlineKeyboardButton> dropProfileRow = new ArrayList<>();
+        InlineKeyboardButton dropProfileButton = new InlineKeyboardButton();
+        dropProfileButton.setText("Drop profile");
+        dropProfileButton.setCallbackData("DROP_PROFILE");
+        dropProfileRow.add(dropProfileButton);
+
+        keyboard.add(changeUsernameRow);
+        keyboard.add(refreshEggsRow);
+        keyboard.add(dropProfileRow);
+        markup.setKeyboard(keyboard);
+        return markup;
+    }
+
+    private InlineKeyboardMarkup getRefreshEggsInlineKeyboardMarkup() {
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+
+        List<InlineKeyboardButton> confirmRow = new ArrayList<>();
+        InlineKeyboardButton yesButton = new InlineKeyboardButton();
+        yesButton.setText("Yes");
+        yesButton.setCallbackData("CONFIRM_REFRESH_EGGS");
+        confirmRow.add(yesButton);
+        InlineKeyboardButton noButton = new InlineKeyboardButton();
+        noButton.setText("No");
+        noButton.setCallbackData("DENY_REFRESH_EGGS");
+        confirmRow.add(noButton);
+
+        keyboard.add(confirmRow);
+        markup.setKeyboard(keyboard);
+        return markup;
+    }
+
+    private InlineKeyboardMarkup getDropProfileInlineKeyboardMarkup() {
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+
+        List<InlineKeyboardButton> confirmRow = new ArrayList<>();
+        InlineKeyboardButton yesButton = new InlineKeyboardButton();
+        yesButton.setText("Yes");
+        yesButton.setCallbackData("CONFIRM_DROP_PROFILE");
+        confirmRow.add(yesButton);
+        InlineKeyboardButton noButton = new InlineKeyboardButton();
+        noButton.setText("No");
+        noButton.setCallbackData("DENY_DROP_PROFILE");
+        confirmRow.add(noButton);
+
+        keyboard.add(confirmRow);
+        markup.setKeyboard(keyboard);
+        return markup;
     }
 }
