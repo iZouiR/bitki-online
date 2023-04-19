6fc191e546a2c3215ce4c7a8ec5eed948518c55a
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-20 00:54:47 +0300

    update BotCommander: add deleteMessage method

--- src/main/java/self/izouir/bitkionline/commander/util/BotCommander.java
+++ src/main/java/self/izouir/bitkionline/commander/util/BotCommander.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendSticker;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -46,6 +47,17 @@ public class BotCommander {
             bot.execute(SendSticker.builder()
                             .chatId(String.valueOf(chatId))
                             .sticker(new InputFile(image.toFile()))
+                    .build());
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public static void deleteMessage(TelegramLongPollingBot bot, Long chatId, Integer messageId) {
+        try {
+            bot.execute(DeleteMessage.builder()
+                    .chatId(String.valueOf(chatId))
+                    .messageId(messageId)
                     .build());
         } catch (TelegramApiException e) {
             log.error(e.getMessage());
