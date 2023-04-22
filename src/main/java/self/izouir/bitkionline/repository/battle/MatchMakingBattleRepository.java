1f31f1337d1709c8d919864f52b718c52c659cd9
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-22 19:09:37 +0300

    update MatchMakingBattleRepository + PrivateBattleRepository: add findAllByPlayerBattle_FirstPlayerOrPlayerBattle_SecondPlayer method

--- src/main/java/self/izouir/bitkionline/repository/battle/MatchMakingBattleRepository.java
+++ src/main/java/self/izouir/bitkionline/repository/battle/MatchMakingBattleRepository.java
@@ -3,7 +3,11 @@ package self.izouir.bitkionline.repository.battle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import self.izouir.bitkionline.entity.battle.MatchMakingBattle;
+import self.izouir.bitkionline.entity.player.Player;
+
+import java.util.List;
 
 @Repository
 public interface MatchMakingBattleRepository extends JpaRepository<MatchMakingBattle, Long> {
+    List<MatchMakingBattle> findAllByPlayerBattle_FirstPlayerOrPlayerBattle_SecondPlayer(Player first, Player second);
 }
