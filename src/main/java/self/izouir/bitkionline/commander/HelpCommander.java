32cc77bc7957feb1a42497b2bcd9a17c24be5d1e
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-22 19:14:19 +0300

    update HelpCommander: add stat-emoji information

--- src/main/java/self/izouir/bitkionline/commander/HelpCommander.java
+++ src/main/java/self/izouir/bitkionline/commander/HelpCommander.java
@@ -18,10 +18,10 @@ public class HelpCommander {
             At beta you enter the game with 3 random eggs
                             
             All the eggs have their characteristics:
-                - Endurance (En) - Describes the max amount of damage the egg could take
-                - Power (Po) - Describes the max amount of damage the egg could deal
-                - Luck (Lu) - Describes the amount of chance to damage enemy egg
-                - Intelligence (In) - Describes the reduction of damage taken while attacking enemy egg
+                - Endurance (❤️‍🩹/🛡) - Describes the max amount of damage the egg could take
+                - Power (💥/💣) - Describes the max amount of damage the egg could deal
+                - Luck (☘️/🍀) - Describes the amount of chance to damage enemy egg
+                - Intelligence (🧿/🪬) - Describes the reduction of damage taken while attacking enemy egg
                       
             Types of battles:
                 Match Making Battle - ranked battle with random opponent based on player's rank
