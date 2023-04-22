273df296b4f06b2fe41328bd34028118d5f3610c
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-22 15:51:34 +0300

    add PlayerBattleRepository

---
+++ src/main/java/self/izouir/bitkionline/repository/battle/PlayerBattleRepository.java
@@ -0,0 +1,9 @@
+package self.izouir.bitkionline.repository.battle;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import self.izouir.bitkionline.entity.battle.PlayerBattle;
+
+@Repository
+public interface PlayerBattleRepository extends JpaRepository<PlayerBattle, Long> {
+}
