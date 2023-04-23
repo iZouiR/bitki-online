3b138cde530852fc6a6a0e7071cfdfa739b7b8a3
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-23 17:21:35 +0300

    rename BotState's states

--- src/main/java/self/izouir/bitkionline/entity/player/BotState.java
+++ src/main/java/self/izouir/bitkionline/entity/player/BotState.java
@@ -1,5 +1,5 @@
 package self.izouir.bitkionline.entity.player;
 
 public enum BotState {
-    AWAIT_USERNAME, CHANGE_USERNAME, AWAIT_LINK
+    AWAIT_USERNAME, AWAIT_NEW_USERNAME, AWAIT_PRIVATE_BATTLE_LINK
 }
