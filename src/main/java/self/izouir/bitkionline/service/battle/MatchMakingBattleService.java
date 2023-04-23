b058813699aa53b7530197a40362f2b198104be1
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-24 02:13:33 +0300

    update MatchMakingBattleService + PrivateBattleService: remove redundant delete methods

--- src/main/java/self/izouir/bitkionline/service/battle/MatchMakingBattleService.java
+++ src/main/java/self/izouir/bitkionline/service/battle/MatchMakingBattleService.java
@@ -1,45 +1,22 @@
 package self.izouir.bitkionline.service.battle;
 
-import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import self.izouir.bitkionline.entity.battle.MatchMakingBattle;
-import self.izouir.bitkionline.entity.battle.PlayerBattle;
-import self.izouir.bitkionline.entity.player.Player;
 import self.izouir.bitkionline.repository.battle.MatchMakingBattleRepository;
-
-import java.util.List;
 
 @Slf4j
 @Service
 public class MatchMakingBattleService {
     private final MatchMakingBattleRepository matchMakingBattleRepository;
-    private final PlayerBattleService playerBattleService;
 
     @Autowired
-    public MatchMakingBattleService(MatchMakingBattleRepository matchMakingBattleRepository,
-                                    PlayerBattleService playerBattleService) {
+    public MatchMakingBattleService(MatchMakingBattleRepository matchMakingBattleRepository) {
         this.matchMakingBattleRepository = matchMakingBattleRepository;
-        this.playerBattleService = playerBattleService;
     }
 
     public void save(MatchMakingBattle matchMakingBattle) {
         matchMakingBattleRepository.save(matchMakingBattle);
-    }
-
-    @Transactional
-    public void delete(MatchMakingBattle matchMakingBattle) {
-        matchMakingBattleRepository.delete(matchMakingBattle);
-    }
-
-    @Transactional
-    public void deleteAllByPlayer(Player player) {
-        List<MatchMakingBattle> matchMakingBattles = matchMakingBattleRepository.findAllByPlayerBattle_FirstPlayerOrPlayerBattle_SecondPlayer(player, player);
-        for (MatchMakingBattle matchMakingBattle : matchMakingBattles) {
-            PlayerBattle battle = matchMakingBattle.getPlayerBattle();
-            playerBattleService.delete(battle);
-            delete(matchMakingBattle);
-        }
     }
 }
--- src/main/java/self/izouir/bitkionline/service/battle/PrivateBattleService.java
+++ src/main/java/self/izouir/bitkionline/service/battle/PrivateBattleService.java
@@ -1,16 +1,12 @@
 package self.izouir.bitkionline.service.battle;
 
-import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import self.izouir.bitkionline.entity.battle.PlayerBattle;
 import self.izouir.bitkionline.entity.battle.PrivateBattle;
-import self.izouir.bitkionline.entity.player.Player;
 import self.izouir.bitkionline.exception.PrivateBattleNotFoundException;
 import self.izouir.bitkionline.repository.battle.PrivateBattleRepository;
 
-import java.util.List;
 import java.util.Random;
 
 @Slf4j
@@ -18,15 +14,12 @@ import java.util.Random;
 public class PrivateBattleService {
     private static final String LINK_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#_";
     private final PrivateBattleRepository privateBattleRepository;
-    private final PlayerBattleService playerBattleService;
     private final Random random;
 
     @Autowired
-    public PrivateBattleService(PrivateBattleRepository privateBattleRepository,
-                                PlayerBattleService playerBattleService) {
+    public PrivateBattleService(PrivateBattleRepository privateBattleRepository) {
         this.random = new Random();
         this.privateBattleRepository = privateBattleRepository;
-        this.playerBattleService = playerBattleService;
     }
 
     public PrivateBattle findByLink(String link) {
@@ -40,22 +33,6 @@ public class PrivateBattleService {
 
     public void save(PrivateBattle privateBattle) {
         privateBattleRepository.save(privateBattle);
-    }
-
-    @Transactional
-    public void delete(PrivateBattle privateBattle) {
-        playerBattleService.delete(privateBattle.getPlayerBattle());
-        privateBattleRepository.delete(privateBattle);
-    }
-
-    @Transactional
-    public void deleteAllByPlayer(Player player) {
-        List<PrivateBattle> privateBattles = privateBattleRepository.findAllByPlayerBattle_FirstPlayerOrPlayerBattle_SecondPlayer(player, player);
-        for (PrivateBattle privateBattle : privateBattles) {
-            PlayerBattle battle = privateBattle.getPlayerBattle();
-            playerBattleService.delete(battle);
-            delete(privateBattle);
-        }
     }
 
     public String generateLink() {
