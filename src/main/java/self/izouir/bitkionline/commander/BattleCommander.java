36c6d06ef0278700bbb4919628f129bf679d95a7
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-23 19:24:48 +0300

    update BattleCommander: continue implementing battle logic

--- src/main/java/self/izouir/bitkionline/commander/BattleCommander.java
+++ src/main/java/self/izouir/bitkionline/commander/BattleCommander.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendSticker;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
@@ -26,11 +27,12 @@ import static self.izouir.bitkionline.util.BotMessageSender.*;
 @Component
 public class BattleCommander {
     private static final Map<Long, Long> CHATS_TO_BATTLES = new ConcurrentHashMap<>();
-    private static final Map<Long, Long> CHATS_TO_MESSAGES = new ConcurrentHashMap<>();
+    private static final Map<Long, Integer> CHATS_TO_MESSAGES = new ConcurrentHashMap<>();
     private static final Map<Long, Long> BATTLES_TO_ATTACKER_CHATS = new ConcurrentHashMap<>();
     private final PlayerBattleService playerBattleService;
     private final EggService eggService;
     private final PlayerService playerService;
+    private final Random random;
 
     @Autowired
     public BattleCommander(PlayerBattleService playerBattleService,
@@ -39,6 +41,7 @@ public class BattleCommander {
         this.playerBattleService = playerBattleService;
         this.eggService = eggService;
         this.playerService = playerService;
+        this.random = new Random();
     }
 
     public void processCallbackQuery(DispatcherBot bot, Long chatId, Integer messageId, String callbackData) {
@@ -64,7 +67,8 @@ public class BattleCommander {
                         battle = playerBattleService.findById(battle.getId());
                     }
 
-                    deleteMessage(bot, chatId, messageId);
+                    CHATS_TO_MESSAGES.put(chatId, messageId);
+                    sendEditMessageText(bot, chatId, messageId, "Battle rages within...");
 
                     Player opponent = battle.getSecondPlayer();
                     Egg opponentEgg = battle.getSecondPlayerEgg();
@@ -90,7 +94,8 @@ public class BattleCommander {
                         battle = playerBattleService.findById(battle.getId());
                     }
 
-                    deleteMessage(bot, chatId, messageId);
+                    CHATS_TO_MESSAGES.put(chatId, messageId);
+                    sendEditMessageText(bot, chatId, messageId, "Battle rages within...");
 
                     Player opponent = battle.getFirstPlayer();
                     Egg opponentEgg = battle.getFirstPlayerEgg();
@@ -107,6 +112,63 @@ public class BattleCommander {
         }
         if (callbackData.equals("BATTLE_OPPONENT_EGG_OK")) {
             deleteMessage(bot, chatId, messageId);
+            messageId = CHATS_TO_MESSAGES.get(chatId);
+            try {
+                sendEditMessageText(bot, chatId, messageId, "Toss a coin...");
+                Thread.sleep(2000);
+
+                for (int i = 3; i > 0; i--) {
+                    Thread.sleep(1000);
+                    sendEditMessageText(bot, chatId, messageId, String.valueOf(i));
+                }
+
+                Player player = playerService.findByChatId(chatId);
+                Player opponent;
+                PlayerBattle battle = playerBattleService.findById(CHATS_TO_BATTLES.get(chatId));
+                if (Objects.equals(player.getId(), battle.getFirstPlayer().getId())) {
+                    opponent = battle.getSecondPlayer();
+                } else {
+                    opponent = battle.getFirstPlayer();
+                }
+
+                if (!BATTLES_TO_ATTACKER_CHATS.containsKey(battle.getId())) {
+                    int chance = random.nextInt(100);
+                    if (chance % 2 == 0) {
+                        BATTLES_TO_ATTACKER_CHATS.put(battle.getId(), chatId);
+                    } else {
+                        BATTLES_TO_ATTACKER_CHATS.put(battle.getId(), opponent.getChatId());
+                    }
+                }
+
+                EditMessageText message = EditMessageText.builder()
+                        .chatId(String.valueOf(chatId))
+                        .messageId(messageId)
+                        .text("Stub")
+                        .build();
+                if (Objects.equals(chatId, BATTLES_TO_ATTACKER_CHATS.get(battle.getId()))) {
+                    message.setText("Your turn to attack, choose the option!");
+                    message.setReplyMarkup(generateAttackerReplyMarkup(player));
+                } else {
+                    message.setText("Your turn to defend, pray to god...");
+                }
+                sendEditMessageText(bot, message);
+            } catch (InterruptedException e) {
+                log.error(e.getMessage());
+            }
+        }
+        if (callbackData.startsWith("BATTLE_ATTACK_")) {
+            String attackType = callbackData.substring("BATTLE_ATTACK_".length());
+            switch (attackType) {
+                case "HARD" -> {
+
+                }
+                case "MEDIUM" -> {
+
+                }
+                case "WEAK" -> {
+
+                }
+            }
         }
     }
 
@@ -149,6 +211,10 @@ public class BattleCommander {
         } catch (InterruptedException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private Integer generateChanceOfAttack(Egg attackerEgg, Egg defenderEgg) {
+        return Math.round((100.0f * attackerEgg.getLuck()) / (attackerEgg.getLuck() + defenderEgg.getLuck()));
     }
 
     private void stopBattleOnDisconnection(DispatcherBot bot, Long chatId) {
@@ -290,6 +356,49 @@ public class BattleCommander {
 
         keyboard.add(opponentRow);
         keyboard.add(okRow);
+        markup.setKeyboard(keyboard);
+        return markup;
+    }
+
+    private InlineKeyboardMarkup generateAttackerReplyMarkup(Player attacker) {
+        PlayerBattle battle = playerBattleService.findById(CHATS_TO_BATTLES.get(attacker.getChatId()));
+        Egg egg;
+        Egg opponentEgg;
+        if (Objects.equals(attacker.getId(), battle.getFirstPlayer().getId())) {
+            egg = battle.getFirstPlayerEgg();
+            opponentEgg = battle.getSecondPlayerEgg();
+        } else {
+            egg = battle.getSecondPlayerEgg();
+            opponentEgg = battle.getFirstPlayerEgg();
+        }
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+
+        List<InlineKeyboardButton> hardAttackRow = new ArrayList<>();
+        InlineKeyboardButton hardAttackButton = new InlineKeyboardButton();
+        hardAttackButton.setText("Hard: " + Math.round(0.8 * egg.getPower())
+                                 + " damage (" + Math.round(0.6 * generateChanceOfAttack(egg, opponentEgg)) + "%)");
+        hardAttackButton.setCallbackData("BATTLE_ATTACK_HARD");
+        hardAttackRow.add(hardAttackButton);
+
+        List<InlineKeyboardButton> mediumAttackRow = new ArrayList<>();
+        InlineKeyboardButton mediumAttackButton = new InlineKeyboardButton();
+        mediumAttackButton.setText("Medium: " + Math.round(0.6 * egg.getPower())
+                                   + " damage (" + Math.round(0.8 * generateChanceOfAttack(egg, opponentEgg)) + "%)");
+        mediumAttackButton.setCallbackData("BATTLE_ATTACK_MEDIUM");
+        mediumAttackRow.add(mediumAttackButton);
+
+        List<InlineKeyboardButton> weakAttackRow = new ArrayList<>();
+        InlineKeyboardButton weakAttackButton = new InlineKeyboardButton();
+        weakAttackButton.setText("Weak: " + Math.round(0.4 * egg.getPower())
+                                 + " damage (" + generateChanceOfAttack(egg, opponentEgg) + "%)");
+        weakAttackButton.setCallbackData("BATTLE_ATTACK_WEAK");
+        weakAttackRow.add(weakAttackButton);
+
+        keyboard.add(hardAttackRow);
+        keyboard.add(mediumAttackRow);
+        keyboard.add(weakAttackRow);
         markup.setKeyboard(keyboard);
         return markup;
     }
