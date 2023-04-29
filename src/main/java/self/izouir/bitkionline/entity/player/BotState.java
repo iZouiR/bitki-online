93d255d9f0085c8de88cbfd6f81352780a4b65ce
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-29 16:34:13 +0300

    refactor BotState: rename to PlayerBotState, add NO_STATE state

--- src/main/java/self/izouir/bitkionline/entity/player/BotState.java
+++
@@ -1,5 +0,0 @@
-package self.izouir.bitkionline.entity.player;
-
-public enum BotState {
-    AWAIT_USERNAME, AWAIT_NEW_USERNAME, AWAIT_PRIVATE_BATTLE_LINK
-}
---
+++ src/main/java/self/izouir/bitkionline/entity/player/PlayerBotState.java
@@ -0,0 +1,8 @@
+package self.izouir.bitkionline.entity.player;
+
+public enum PlayerBotState {
+    NO_STATE,
+    AWAIT_USERNAME,
+    AWAIT_NEW_USERNAME,
+    AWAIT_PRIVATE_BATTLE_LINK
+}
