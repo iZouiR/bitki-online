c54f456606eebe6b840621f939a63369f7502561
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-29 13:40:05 +0300

    update MatchMakingBattleRepository + PrivateBattleRepository: remove redundant methods

--- src/main/java/self/izouir/bitkionline/repository/battle/PrivateBattleRepository.java
+++ src/main/java/self/izouir/bitkionline/repository/battle/PrivateBattleRepository.java
@@ -3,14 +3,10 @@ package self.izouir.bitkionline.repository.battle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import self.izouir.bitkionline.entity.battle.PrivateBattle;
-import self.izouir.bitkionline.entity.player.Player;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PrivateBattleRepository extends JpaRepository<PrivateBattle, Long> {
     Optional<PrivateBattle> findByLink(String link);
-
-    List<PrivateBattle> findAllByPlayerBattle_FirstPlayerOrPlayerBattle_SecondPlayer(Player first, Player second);
 }
