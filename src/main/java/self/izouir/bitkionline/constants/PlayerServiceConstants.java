02d6487d41c700fae2a5053d1227f582163203a4
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-30 01:21:33 +0300

    update PlayerServiceConstants

--- src/main/java/self/izouir/bitkionline/constants/PlayerServiceConstants.java
+++ src/main/java/self/izouir/bitkionline/constants/PlayerServiceConstants.java
@@ -4,6 +4,9 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class PlayerServiceConstants {
+    public static final Integer MINIMUM_USERNAME_LENGTH = 4;
+    public static final Integer MAXIMUM_USERNAME_LENGTH = 16;
+    public static final String USERNAME_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String NOT_REGISTERED_PLAYER_USERNAME = "new_player_%s";
     public static final Integer NOT_REGISTERED_PLAYER_RANK = 0;
     public static final Boolean NOT_REGISTERED_PLAYER_IS_PLAYING = false;
