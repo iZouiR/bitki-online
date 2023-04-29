66c09a7de6cde8b7af81bb9fd825303829b328a6
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-29 14:38:26 +0300

    update PlayerBot: add validation annotations

--- src/main/java/self/izouir/bitkionline/entity/player/PlayerBot.java
+++ src/main/java/self/izouir/bitkionline/entity/player/PlayerBot.java
@@ -1,6 +1,7 @@
 package self.izouir.bitkionline.entity.player;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,9 +26,11 @@ public class PlayerBot {
     @Column(name = "last_bot_state")
     private BotState lastBotState;
 
+    @PositiveOrZero
     @Column(name = "last_inventory_index")
     private Integer lastInventoryIndex;
 
+    @PositiveOrZero
     @Column(name = "last_inventory_size")
     private Integer lastInventorySize;
 }
