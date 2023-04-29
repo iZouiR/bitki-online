3bc91410353495abddf1117dfa25614f0942ef86
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-29 17:48:04 +0300

    add BotMessageSenderConstants

---
+++ src/main/java/self/izouir/bitkionline/constants/BotMessageSenderConstants.java
@@ -0,0 +1,14 @@
+package self.izouir.bitkionline.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class BotMessageSenderConstants {
+    public static final String GREETINGS_MESSAGE = "Christ is risen, %s!";
+    public static final String AWAIT_USERNAME_MESSAGE = "Enter your username";
+    public static final String PLAYER_NOT_REGISTERED_MESSAGE = "Finish registration before continuing";
+    public static final String PLAYER_REGISTERED_MESSAGE = "You're now registered with username %s";
+    public static final String PLAYER_ALREADY_EXISTS_MESSAGE = "Player with username %s already exists, try another variant";
+
+    public static final String OBTAINING_EGG_MESSAGE = "You obtained %s egg (%s)";
+}
