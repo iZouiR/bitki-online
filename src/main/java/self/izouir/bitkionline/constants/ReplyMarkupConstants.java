48f700178e7eca08354ecb70a49fafaf63412b6b
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-29 20:00:49 +0300

    add ReplyMarkupConstants

---
+++ src/main/java/self/izouir/bitkionline/constants/ReplyMarkupConstants.java
@@ -0,0 +1,9 @@
+package self.izouir.bitkionline.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ReplyMarkupConstants {
+    public static final String REFRESH_TEXT = "Refresh";
+    public static final String CLOSE_TEXT = "Close";
+}
