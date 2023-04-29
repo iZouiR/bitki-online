ab7734d4d73fe5f6c95e57b2ccf5e86ff9bc9480
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-29 17:42:24 +0300

    add constants package: add EggServiceConstants

---
+++ src/main/java/self/izouir/bitkionline/constants/EggServiceConstants.java
@@ -0,0 +1,45 @@
+package self.izouir.bitkionline.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class EggServiceConstants {
+    public static final String BASE_EGG_IMAGE_PATH = "src\\main\\resources\\image\\egg\\";
+    public static final Integer START_INVENTORY_SIZE = 3;
+    public static final Integer EGG_GENERATION_CHANCE = 100;
+    public static final Integer WEAK_EGG_GENERATION_CHANCE = 80;
+    public static final Integer STRONG_EGG_GENERATION_CHANCE = 15;
+    public static final Boolean NEW_EGG_IS_CRACKED = false;
+
+    public static final Integer WEAK_EGG_MINIMUM_ENDURANCE = 8;
+    public static final Integer WEAK_EGG_MAXIMUM_ENDURANCE = 16;
+    public static final Integer WEAK_EGG_MINIMUM_POWER = 3;
+    public static final Integer WEAK_EGG_MAXIMUM_POWER = 5;
+    public static final Integer WEAK_EGG_MINIMUM_LUCK = 1;
+    public static final Integer WEAK_EGG_MAXIMUM_LUCK = 5;
+    public static final Integer WEAK_EGG_MINIMUM_INTELLIGENCE = 0;
+    public static final Integer WEAK_EGG_MAXIMUM_INTELLIGENCE = 5;
+
+    public static final Integer STRONG_EGG_MINIMUM_ENDURANCE = 10;
+    public static final Integer STRONG_EGG_MAXIMUM_ENDURANCE = 18;
+    public static final Integer STRONG_EGG_MINIMUM_POWER = 4;
+    public static final Integer STRONG_EGG_MAXIMUM_POWER = 6;
+    public static final Integer STRONG_EGG_MINIMUM_LUCK = 2;
+    public static final Integer STRONG_EGG_MAXIMUM_LUCK = 6;
+    public static final Integer STRONG_EGG_MINIMUM_INTELLIGENCE = 1;
+    public static final Integer STRONG_EGG_MAXIMUM_INTELLIGENCE = 6;
+
+    public static final Integer HOLY_EGG_MINIMUM_ENDURANCE = 12;
+    public static final Integer HOLY_EGG_MAXIMUM_ENDURANCE = 20;
+    public static final Integer HOLY_EGG_MINIMUM_POWER = 5;
+    public static final Integer HOLY_EGG_MAXIMUM_POWER = 7;
+    public static final Integer HOLY_EGG_MINIMUM_LUCK = 3;
+    public static final Integer HOLY_EGG_MAXIMUM_LUCK = 7;
+    public static final Integer HOLY_EGG_MINIMUM_INTELLIGENCE = 2;
+    public static final Integer HOLY_EGG_MAXIMUM_INTELLIGENCE = 7;
+
+    public static final Integer GREAT_ENDURANCE = 16;
+    public static final Integer GREAT_POWER = 5;
+    public static final Integer GREAT_LUCK = 5;
+    public static final Integer GREAT_INTELLIGENCE = 5;
+}
