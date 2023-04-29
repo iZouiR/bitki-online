d81ec68fce473ccba91751ecfeb3a2f4089e2056
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-29 17:45:30 +0300

    add PlayerServiceConstants

---
+++ src/main/java/self/izouir/bitkionline/constants/PlayerServiceConstants.java
@@ -0,0 +1,10 @@
+package self.izouir.bitkionline.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PlayerServiceConstants {
+    public static final String NOT_REGISTERED_PLAYER_USERNAME = "new_player_%s";
+    public static final Integer NOT_REGISTERED_PLAYER_RANK = 0;
+    public static final Boolean NOT_REGISTERED_PLAYER_IS_PLAYING = false;
+}
