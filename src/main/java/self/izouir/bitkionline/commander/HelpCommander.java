33292611fda54a4b2ce92f444ea89ef8c955f3b9
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-29 21:52:49 +0300

    refactor HelpCommander: remove magic vars

--- src/main/java/self/izouir/bitkionline/commander/HelpCommander.java
+++ src/main/java/self/izouir/bitkionline/commander/HelpCommander.java
@@ -9,38 +9,13 @@ import self.izouir.bitkionline.bot.DispatcherBot;
 import java.util.ArrayList;
 import java.util.List;
 
+import static self.izouir.bitkionline.constants.BotMessageSenderConstants.HELP_MESSAGE;
+import static self.izouir.bitkionline.constants.ReplyMarkupConstants.CLOSE;
 import static self.izouir.bitkionline.util.BotMessageSender.deleteMessage;
 import static self.izouir.bitkionline.util.BotMessageSender.sendMessage;
 
 @Component
 public class HelpCommander {
-    private static final String HELP_MESSAGE = """
-            At beta you enter the game with 3 random eggs
-                            
-            All the eggs have their characteristics:
-                - Endurance (❤️‍🩹/🛡) - Describes the max amount of damage the egg could take, influences Power (💥/💣)
-                - Power (💥/💣) - Describes the amount of damage the egg could deal
-                - Luck (☘️/🍀) - Describes the amount of chance to damage enemy's egg
-                - Intelligence (🧿/🪬) - Describes the reduction of reply damage taken while attacking enemy egg
-                      
-            Types of battles:
-                Match Making Battle - battle with random opponent
-                Private Battle - battle via link
-                
-            Rules of game:
-                1. The battle starts by choosing who would attack first - the coin flip
-                2. Then attacker choose one of 3 variants of attack
-                    (higher damage - lower chance)
-                    (medium damage - medium chance)
-                    (lower damage - higher chance)
-                3. Then chance and damage applies, calculating endurance of both battling eggs
-                4. Turn goes to the next player and points 2-4 become cycled
-                5. One or both eggs are broken
-                    (one broken egg belongs to defeated player)
-                    (both eggs are broken - draw)
-                6. Calculates awards of winner, punishments of looser
-            """;
-
     public void processCallbackQuery(DispatcherBot bot, Long chatId, Integer messageId, String callbackData) {
         if (callbackData.equals("HELP_CLOSE")) {
             deleteMessage(bot, chatId, messageId);
@@ -62,7 +37,7 @@ public class HelpCommander {
 
         List<InlineKeyboardButton> closeRow = new ArrayList<>();
         InlineKeyboardButton closeButton = new InlineKeyboardButton();
-        closeButton.setText("Close");
+        closeButton.setText(CLOSE);
         closeButton.setCallbackData("HELP_CLOSE");
         closeRow.add(closeButton);
 
