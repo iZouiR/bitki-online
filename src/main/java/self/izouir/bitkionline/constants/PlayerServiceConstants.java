3a844935e562bb3aa20183096e476fd0d536341c
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-29 20:00:25 +0300

    update PlayerServiceConstants

--- src/main/java/self/izouir/bitkionline/constants/PlayerServiceConstants.java
+++ src/main/java/self/izouir/bitkionline/constants/PlayerServiceConstants.java
@@ -7,4 +7,10 @@ public class PlayerServiceConstants {
     public static final String NOT_REGISTERED_PLAYER_USERNAME = "new_player_%s";
     public static final Integer NOT_REGISTERED_PLAYER_RANK = 0;
     public static final Boolean NOT_REGISTERED_PLAYER_IS_PLAYING = false;
+
+    public static final Integer LEADERS_COUNT = 10;
+    public static final String LEADER_RANK_INFO = "%s - %s \uD83C\uDF96\n";
+    public static final String PLAYER_RANK_INFO = "You are top-%d player with %d points";
+    public static final String EMPTY_RANK_INFO = "There are no players at all, come back later";
+    public static final String RANK_INFO_SEPARATOR = "--------------------------------------------------------------\n";
 }
