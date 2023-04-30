ec211d5ceda6e81ca1d08ce8eb34bb533327d3df
Timofey Kozlov <iZouiR@jotaro.ru>
2023-04-30 15:28:27 +0300

    update EggRepository: add unbindAllByOwner method

--- src/main/java/self/izouir/bitkionline/repository/egg/EggRepository.java
+++ src/main/java/self/izouir/bitkionline/repository/egg/EggRepository.java
@@ -1,6 +1,9 @@
 package self.izouir.bitkionline.repository.egg;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import self.izouir.bitkionline.entity.egg.Egg;
 import self.izouir.bitkionline.entity.player.Player;
@@ -12,4 +15,8 @@ public interface EggRepository extends JpaRepository<Egg, Long> {
     List<Egg> findAllByOwner(Player owner);
 
     void deleteAllByOwner(Player owner);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE Egg SET owner = null WHERE owner = :owner")
+    void unbindAllByOwner(@Param("owner") Player owner);
 }
