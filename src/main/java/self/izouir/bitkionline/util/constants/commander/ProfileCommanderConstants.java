57750f5dd77bdebc869e8e262020593ef1481877
Timofey Kozlov <iZouiR@jotaro.ru>
2023-05-01 18:57:00 +0300

    update ProfileCommanderConstants

--- src/main/java/self/izouir/bitkionline/util/constants/commander/ProfileCommanderConstants.java
+++ src/main/java/self/izouir/bitkionline/util/constants/commander/ProfileCommanderConstants.java
@@ -7,6 +7,20 @@ import static self.izouir.bitkionline.util.constants.service.PlayerServiceConsta
 
 @UtilityClass
 public class ProfileCommanderConstants {
+    public static final String STATISTICS_MESSAGE = """
+            Win rate - (%s%%) 🎲
+            Head attack success rate - (%s%%) 🎰
+            Side attack success rate - (%s%%) 🎰
+            Ass attack success rate - (%s%%) 🎰
+            Total damage dealt - %s 💥
+            Total damage taken - %s 💥
+            Total rank points earned - %s 🎖
+            Total rank points lost - %s 🎖
+            Total eggs obtained - %s 🔮
+            Holy eggs obtained - %s 🔮
+            Strong eggs obtained - %s 🔮
+            Weak eggs obtained - %s 🔮
+            """;
     public static final String AWAIT_NEW_USERNAME_MESSAGE = "Enter your new username, it must contains english/russian letters + digits + '_' and be between "
                                                             + MINIMUM_USERNAME_LENGTH + " and " + MAXIMUM_USERNAME_LENGTH + " characters in length";
     public static final String USERNAME_CHANGE_SUCCESS_MESSAGE = "Success, you were renamed to %s";
@@ -15,6 +29,7 @@ public class ProfileCommanderConstants {
     public static final String PROFILE_DROP_CONFIRMATION_MESSAGE = "Are you sure you want to drop your profile?";
     public static final String PROFILE_DROP_SUCCESS_MESSAGE = "Your profile was dropped successfully";
 
+    public static final String STATISTICS_BUTTON_TEXT = "Statistics";
     public static final String CHANGE_USERNAME_BUTTON_TEXT = "Change username";
     public static final String REFRESH_EGGS_BUTTON_TEXT = "Refresh eggs";
     public static final String DROP_PROFILE_BUTTON_TEXT = "Drop profile";
