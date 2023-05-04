bb6648b25ae74287b0fd5f25e194f2a547577eea
Timofey Kozlov <iZouiR@jotaro.ru>
2023-05-04 15:31:46 +0300

    refactor SupportMessageService: add @RequiredArgsConstructor annotation

--- src/main/java/self/izouir/bitkionline/service/SupportMessageService.java
+++ src/main/java/self/izouir/bitkionline/service/SupportMessageService.java
@@ -1,6 +1,6 @@
 package self.izouir.bitkionline.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import self.izouir.bitkionline.entity.SupportMessage;
 import self.izouir.bitkionline.entity.player.Player;
@@ -11,19 +11,13 @@ import self.izouir.bitkionline.service.player.PlayerBotService;
 import java.sql.Timestamp;
 import java.time.Instant;
 
-import static self.izouir.bitkionline.util.constants.commander.SupportCommanderConstants.*;
+import static self.izouir.bitkionline.util.constant.commander.SupportCommanderConstant.*;
 
+@RequiredArgsConstructor
 @Service
 public class SupportMessageService {
     private final PlayerBotService playerBotService;
     private final SupportMessageRepository supportMessageRepository;
-
-    @Autowired
-    public SupportMessageService(PlayerBotService playerBotService,
-                                 SupportMessageRepository supportMessageRepository) {
-        this.playerBotService = playerBotService;
-        this.supportMessageRepository = supportMessageRepository;
-    }
 
     public void save(SupportMessage supportMessage) {
         supportMessageRepository.save(supportMessage);
