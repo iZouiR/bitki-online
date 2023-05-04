e23cf56bf18dfe9239ac027c6fb0481a08dc6bce
Timofey Kozlov <iZouiR@jotaro.ru>
2023-05-04 15:29:33 +0300

    refactor MatchMakingBattleService: add @RequiredArgsConstructor annotation

--- src/main/java/self/izouir/bitkionline/service/battle/MatchMakingBattleService.java
+++ src/main/java/self/izouir/bitkionline/service/battle/MatchMakingBattleService.java
@@ -1,24 +1,18 @@
 package self.izouir.bitkionline.service.battle;
 
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import self.izouir.bitkionline.entity.battle.MatchMakingBattle;
 import self.izouir.bitkionline.entity.battle.PlayerBattle;
 import self.izouir.bitkionline.entity.player.Player;
 import self.izouir.bitkionline.repository.battle.MatchMakingBattleRepository;
 
+@RequiredArgsConstructor
 @Service
 public class MatchMakingBattleService {
     private final PlayerBattleService playerBattleService;
     private final MatchMakingBattleRepository matchMakingBattleRepository;
-
-    @Autowired
-    public MatchMakingBattleService(PlayerBattleService playerBattleService,
-                                    MatchMakingBattleRepository matchMakingBattleRepository) {
-        this.playerBattleService = playerBattleService;
-        this.matchMakingBattleRepository = matchMakingBattleRepository;
-    }
 
     public void save(MatchMakingBattle matchMakingBattle) {
         matchMakingBattleRepository.save(matchMakingBattle);
