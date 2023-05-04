db09e9c4b4cb211b27b891de10f2c4b657d8f419
Timofey Kozlov <iZouiR@jotaro.ru>
2023-05-04 15:29:03 +0300

    refactor BotConfig: add @RequiredArgsConstructor annotation

--- src/main/java/self/izouir/bitkionline/config/BotConfig.java
+++ src/main/java/self/izouir/bitkionline/config/BotConfig.java
@@ -1,8 +1,8 @@
 package self.izouir.bitkionline.config;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -11,15 +11,11 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import self.izouir.bitkionline.bot.DispatcherBot;
 
+@RequiredArgsConstructor
 @Configuration
 public class BotConfig {
     private static final Logger LOGGER = LoggerFactory.getLogger(BotConfig.class);
     private final DispatcherBot dispatcherBot;
-
-    @Autowired
-    public BotConfig(DispatcherBot dispatcherBot) {
-        this.dispatcherBot = dispatcherBot;
-    }
 
     @EventListener({ContextRefreshedEvent.class})
     public void init() {
